d2e408d72b6b93dc3078290990019788bbaec6af
PhuongQuyen0908 <23521327@gm.uit.edu.vn>
2025-05-17 23:34:28 +0700

    first_commit

---
+++ src/dataset.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+data = {
+    "age": [23, 45, 12, 36, 29],
+    "name": ["Alice", "Bob", "Charlie", "David", "Eve"],
+    "city": ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"],
+    "salary": [70000, 80000, 50000, 60000, 90000],
+}
+
+df = pd.DataFrame(data)
